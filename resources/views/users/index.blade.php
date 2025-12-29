ccc18184ef5daac182ac1305b0eaef329f84457e
Guilherme Rocha <guilherme.rocha@ufvjm.edu.br>
2025-12-29 14:24:27 -0300

    feat: criando paginação na lista de usuários

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,24 +3,24 @@
 @section('pagetitle', 'Usuários')
 
 @section('main')
-<h1>Hello World</h1>
-<p>{{ $greeting }}</p>
+  <h1>Hello World</h1>
+  <p>{{ $greeting }}</p>
 
-<figure>
-  <img src="{{ Vite::asset('resources/images/github_profile.jpeg') }}" alt="" />
-</figure>
+  <figure>
+    <img src="{{ Vite::asset('resources/images/github_profile.jpeg') }}" alt="" />
+  </figure>
 
-<ul>
-  @foreach ($users as $user)
+  <ul>
+    @foreach ($users as $user)
+      @php
+        $name = $user->name;
+        $email = $user->email;
+      @endphp
 
-  @php
-  $name = $user->name;
-  $email = $user->email;
-  @endphp
-
-  <li>
-    {{ $name }} ({{ $email }})
-  </li>
-  @endforeach
-</ul>
+      <li>
+        {{ $name }} ({{ $email }})
+      </li>
+    @endforeach
+  </ul>
+  {{ $users->links() }}
 @endsection
